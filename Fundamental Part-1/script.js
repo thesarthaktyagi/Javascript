f494a8d4adbdd905eeb29504d445c271f812d1bc
Sarthak Tyagi <srthk07@gmail.com>
2020-11-20 11:41:48 +0530

    First commit �

---
+++ Fundamental Part-1/script.js
@@ -0,0 +1,46 @@
+/*
+let country = 'India';
+let continent = 'Asia';
+let population = 1350;
+
+console.log("Country is :", country, "Continent is :", continent, "Population is :", population)
+
+*/
+
+// console.log(true);
+
+// let jsIsFun = true;
+
+// console.log(jsIsFun);
+
+// console.log(typeof jsIsFun);
+
+// jsIsFun = 'YES!';
+
+// console.log(typeof jsIsFun);
+
+/*
+let status;
+
+console.log(status);
+console.log(typeof status);
+
+status = 'Pass';
+
+console.log(status);
+
+console.log(typeof status);
+
+console.log(typeof null);
+*/
+
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof language);
+
+const ageSarthak = 2021 - 1998;
+console.log(ageSarthak);
+
+
