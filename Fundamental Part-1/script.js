680dc1764890df86030e10e6eb5229d461cc5017
Sarthak Tyagi <srthk07@gmail.com>
2020-11-28 12:12:51 +0530

    Fundamentals part 1 done

--- Fundamental Part-1/script.js
+++ Fundamental Part-1/script.js
@@ -40,7 +40,166 @@ console.log(typeof null);
 // console.log(typeof isIsland);
 // console.log(typeof language);
 
-const ageSarthak = 2021 - 1998;
-console.log(ageSarthak);
+// const ageSarthak = 2021 - 1998;
+// console.log(ageSarthak);
+
+/*
+const markWeight = 78;
+const markHeight = 1.69;
+
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+const markBMI = markWeight / markHeight ** 2;
+console.log(markBMI);
+const johnBMI = johnWeight / johnHeight ** 2;
+console.log(johnBMI);
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+
+*/
+
+/*
+const firstname = 'Sarthak';
+const job = 'Student';
+const birthYear = 1999;
+const year = 2020;
+
+const sentence = `I'am ${firstname}, a ${year - birthYear} year old ${job}!`;
+console.log(sentence);
 
 
+console.log(`Just a regular string...`);
+
+
+console.log(`String
+multiple 
+lines`)
+*/
+
+/*
+const age = 17;
+
+if(age >= 18) {
+    console.log('Sarah can start driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years`);
+}
+
+*/
+
+/*
+const birthYear = 2001;
+let century;
+
+if(birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
+*/
+
+/*
+const markWeight = 68;
+const markHeight = 1.69;
+
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / johnHeight ** 2;
+
+if(markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`);
+}
+
+*/
+
+
+// const age = 18;
+
+// if (age === 18) {
+//     console.log('You just become an adult')
+// }
+
+// const age = Number(prompt("What is your age ?"));
+// console.log(age);
+// console.log(typeof age);
+
+// if (age === 21) {
+//     console.log('You are cool !');
+// } else if (age === 22) {
+//     console.log('You are also cool !');
+// } else {
+//     console.log('You are just fool !');
+// }
+
+
+// if (age !== 23) {
+//     console.log('Age is not 23');
+// }
+
+/*
+const dolphinsAverage = (97 + 112 + 101) / 3;
+const koalasAverage = (109 + 95 + 106) / 3;
+console.log(`dolphins score ${dolphinsAverage}`)
+console.log(`koalas score ${koalasAverage}`)
+
+if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+    console.log('Dolphins won the match');
+} else if (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
+    console.log('Koalas won the match');
+} else if (dolphinsAverage === koalasAverage && dolphinsAverage >= 100 & koalasAverage >= 100) {
+    console.log('Match is drawn');
+} else {
+    console.log('No team won the match');
+}
+*/
+/*
+const day = 'wednesday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Coding for monday');
+        break;
+    case 'tuesday':
+        console.log('coding for tuesday');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Just coding');
+        break;
+
+    case 'friday':
+        console.log('coding for friday');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend bro');
+        break;
+
+    default:
+        console.log('Not a valid day');
+}
+*/
+
+
+// const age = 12;
+
+// console.log(`You ${age >= 18 ? 'can drive 🚗' : 'cannot drive 🚗'}`);
+
+/*
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value ${bill + tip}`)
+
+*/
